455af37b01be47d572d11d991c481d9062e32b85
Joenilo Estillero <joeniloe@gmail.com>
2025-10-29 11:39:54 +0800

    Change database password to empty string
    Updated database password to an empty string for security.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio',
         "USER": 'postgres',
-        'PASSWORD': '232323',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
